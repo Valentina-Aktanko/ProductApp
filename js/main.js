af5a9569aef639e7c6651bec30f4470ad5b4d409
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-07-15 18:05:29 +0300

    feat: attribute binding

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks'
+    product: 'Socks',
+    description: 'A pair of warm, fuzzy socks',
+    image: './assets/vmSocks-green.jpg',
+    altText: 'Green Vue Mastery socks',
+    href: '#'
   }
 })
