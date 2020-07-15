a70205fdb0ec5831e4c3788af23d52bf7e7b4dc4
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-07-15 19:17:27 +0300

    feat: list rendering

--- js/main.js
+++ js/main.js
@@ -5,8 +5,31 @@ var app = new Vue({
     description: 'A pair of warm, fuzzy socks',
     image: './assets/vmSocks-green.jpg',
     altText: 'Green Vue Mastery socks',
-    href: '#',
     inventory: 100,
-    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ],
+    sizes: [
+      {
+        sizeId: 0,
+        sizeValue: 'S'
+      },
+      {
+        sizeId: 1,
+        sizeValue: 'M'
+      },
+      {
+        sizeId: 2,
+        sizeValue: 'L'
+      },
+    ]
   }
 })
