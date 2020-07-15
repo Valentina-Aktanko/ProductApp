4f2ee9cb8337a67091e687b66d2b3ef9d054c2a6
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-07-15 18:56:41 +0300

    feat: conditional rendering

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ var app = new Vue({
     description: 'A pair of warm, fuzzy socks',
     image: './assets/vmSocks-green.jpg',
     altText: 'Green Vue Mastery socks',
-    href: '#'
+    href: '#',
+    inventory: 100,
+    onSale: true,
   }
 })
